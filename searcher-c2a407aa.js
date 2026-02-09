2e1950ea155170180e4cbaff7ac2a64ea96b495c
sandeepkunkunuru <sandeepkunkunuru@users.noreply.github.com>
2026-02-09 02:05:09 +0000

    deploy: ab9bfe89b5e2661736601c222c6f55e05fc878da

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-30301058.js',
+                path_to_root + 'searchindex-a03f5f1e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
